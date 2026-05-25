7d4bf546f2e2fe95c5241a730204c630a3cd4284
MAR089-prog <scratch-mark-markos@yandex.ru>
2026-05-25 13:50:31 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const inputField = document.querySelector('textarea');
 const supabaseClient = supabase.createClient('https://sbhldxkihrratikvypim.supabase.co/rest/v1/', 'sb_publishable_KL71ZxnAu7iIbENPRmK9Ag_FVfucdLw')
+const publishBtn = document.getElementById('publish-btn');
+
 
 
 async function sendPost() {
   const text = inputField.value;
   await supabaseClient.from('posts').insert([{ text: text }]);
 }
+
+publishBtn.addEventListener('click', sendPost);
+
